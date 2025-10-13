bcd2fb35360acf2b9504453d9f2e06ad9904bed2
Neeraj Raghav <thynk.neeraj@gmail.com>
2025-10-13 17:03:17 +0530

    update app.tsx girl rename.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "@/components/page/hero/hero";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Mid } from "@/components/page/mid/mid";
-import Girl from "@/components/page/hero/Girl"; 
+import Girl from "@/components/page/hero/girl"; 
 import Popular from"@/components/page/hero/Popular";
  // ← Add /hero/
 
